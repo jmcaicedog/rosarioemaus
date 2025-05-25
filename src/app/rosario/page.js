fdd03b5ef391480e16a9701f6121714a0047fd66
Juan Manuel Caicedo <juanmanuelcaicedo@hotmail.com>
2025-05-25 00:16:50 -0500

    Stepper added on Rosary page

--- src/app/rosario/page.js
+++ src/app/rosario/page.js
@@ -1,13 +1,12 @@
 "use client";
 import Header from "@/components/Header";
+import VerticalLinearStepper from "@/components/VerticalLinearStepper";
 
 export default function Page() {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-center justify-between">
-        <h1 className="">Rosario</h1>
-      </main>
+      <VerticalLinearStepper />
     </>
   );
 }
