6ef5bdec6a88e0feabad10fda2b9a42c284595fe
Juan Manuel Caicedo <juanmanuelcaicedo@hotmail.com>
2025-06-17 17:05:41 -0500

    Stepper Styles Updated

--- src/components/VerticalLinearStepper.js
+++ src/components/VerticalLinearStepper.js
@@ -8,12 +8,24 @@ import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 
+const DiaActual = () => {
+  const getDiaDelAño = () => {
+    const hoy = new Date();
+    const inicioDelAño = new Date(hoy.getFullYear(), 0, 1);
+    const diferencia = hoy - inicioDelAño;
+    const unDia = 1000 * 60 * 60 * 24;
+    return Math.floor(diferencia / unDia) + 1;
+  };
+
+  return getDiaDelAño();
+};
+
+const dia = DiaActual();
+
 const steps = [
   {
-    label: "Select campaign settings",
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
+    label: "Hora de María",
+    description: `Si alguno de los asistentes tiene en sus manos La Hora de María, puede hacer la lectura correspondiente al día ${dia}`,
   },
   {
     label: "Create an ad group",
@@ -45,35 +57,56 @@ export default function VerticalLinearStepper() {
   };
 
   return (
-    <Box sx={{ maxWidth: 400 }}>
+    <Box sx={{ maxWidth: 900, padding: 4, fontFamily: "inherit" }}>
       <Stepper activeStep={activeStep} orientation="vertical">
         {steps.map((step, index) => (
           <Step key={step.label}>
             <StepLabel
-              optional={
-                index === steps.length - 1 ? (
-                  <Typography variant="caption">Last step</Typography>
-                ) : null
-              }
+              sx={{
+                ".MuiStepLabel-label": {
+                  fontFamily: "inherit", // Hereda la tipografía del sitio
+                  fontSize: "1rem", // Opcional: aseguras que el tamaño también sea consistente
+                  color: "#A12330", // Hereda el color del contenedor o body
+                  fontWeight: "bold",
+                },
+                ".Mui-active .MuiStepLabel-label": {
+                  fontWeight: "bold", // Si quieres marcar la activa, también hereda tipografía
+                },
+                ".MuiStepIcon-root": {
+                  color: "#bdbdbd", // color del disco (paso no activo, no completado)
+                },
+                ".MuiStepIcon-root.Mui-active": {
+                  color: "#A12330", // color del disco del paso activo
+                },
+                ".MuiStepIcon-root.Mui-completed": {
+                  color: "#26490f", // color del disco del paso completado
+                },
+              }}
             >
               {step.label}
             </StepLabel>
-            <StepContent>
-              <Typography>{step.description}</Typography>
-              <Box sx={{ mb: 2 }}>
+            <StepContent sx={{ fontFamily: "inherit" }}>
+              <h3>{step.description}</h3>
+              <Box sx={{ mb: 2, typography: "inherit" }}>
                 <Button
                   variant="contained"
                   onClick={handleNext}
-                  sx={{ mt: 1, mr: 1 }}
+                  sx={{ mt: 1, mr: 1, bgcolor: "#A12330" }}
                 >
-                  {index === steps.length - 1 ? "Finish" : "Continue"}
+                  {index === steps.length - 1 ? "Finalizar" : "Siguiente"}
                 </Button>
                 <Button
                   disabled={index === 0}
                   onClick={handleBack}
-                  sx={{ mt: 1, mr: 1 }}
+                  sx={{
+                    mt: 1,
+                    mr: 1,
+                    typography: "inherit",
+                    color: "#A12330",
+                    fontWeight: "bold",
+                  }}
                 >
-                  Back
+                  ANTERIOR
                 </Button>
               </Box>
             </StepContent>
@@ -81,10 +114,16 @@ export default function VerticalLinearStepper() {
         ))}
       </Stepper>
       {activeStep === steps.length && (
-        <Paper square elevation={0} sx={{ p: 3 }}>
-          <Typography>All steps completed - you&apos;re finished</Typography>
-          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-            Reset
+        <Paper square elevation={0} sx={{ p: 3, fontFamily: "inherit" }}>
+          <Typography sx={{ fontFamily: "inherit" }}>
+            Ahora vamos a meditar los misterios del día
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={handleReset}
+            sx={{ mt: 1, mr: 1, typography: "inherit", bgcolor: "#A12330" }}
+          >
+            CONTINUAR
           </Button>
         </Paper>
       )}
